3eaa274b37fd0320883ee2399c90965aa4d5a32c
Siddesh Pillai <siddeshvijji@gmail.com>
2018-12-31 10:57:36 -0800

    Remove Overdrawing

---
+++ src/main/java/com/example/android/mobileperf/render/ChatumLatinumActivity.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.mobileperf.render;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Displays a fragment that contains a ListView holding several Chat objects. Used in this lesson
+ * to show a) how to reduce overdraws, and b) how to flatten needlessly nested hierarchies.
+ */
+public class ChatumLatinumActivity extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatum_latinum);
+
+        // The Theme's window background is masked by the opague background of the activity and
+        // causes unnecessary overdraw. Nullifying the window background removed the overdraw
+
+        getWindow().setBackgroundDrawable(null);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.activity_chatum_latinum_container, new ChatsFragment())
+                    .commit();
+        }
+    }
+}
